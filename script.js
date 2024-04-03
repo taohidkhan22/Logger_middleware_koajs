a53988d1ebf5f5ad4a867d3c08140259155f3bcd
taohidkhan22 <taohid130@gmail.com>
2024-04-03 17:36:57 +0600

    xml response object

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const app=new koa();
 app.use(async(ctx, next) =>{
     ctx.body="in port 8085 the server is runnign";
 } );
+app.use(async (ctx, next) => {
+  await next();
+  ctx.response.type = 'xml';
+  ctx.response.body = fs.createReadStream('really_large.xml');
+});
 app.listen(8085, ()=>{
     console.log('runnign app');
 })
